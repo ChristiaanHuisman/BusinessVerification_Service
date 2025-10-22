c9e2852a3767f685c940fd956b2cb833b195c9be
Christiaan Huisman <huismanchristiaanwork@gmail.com>
2025-10-22 20:57:45 +0200

    Setup Firestore credentials and connection

--- BusinessVerification_Service.Api/Program.cs
+++ BusinessVerification_Service.Api/Program.cs
@@ -1,3 +1,6 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
 
 namespace BusinessVerification_Service.Api
 {
@@ -6,6 +9,30 @@ namespace BusinessVerification_Service.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            try
+            {
+                // Register FirestoreDb as a Singleton
+                // Only done once per application lifetime as it is for prototyping purposes
+                // Having periodic refresh of the credentials can be added in future versions, 
+                // for performance improvements when the application scales to a more permanent hosting solution
+                var credentialPath = builder.Configuration["Firestore:CredentialsPath"];
+                var projectId = builder.Configuration["Firestore:ProjectId"];
+                var googleCredential = GoogleCredential.FromFile(credentialPath);
+                var firestoreClient = new FirestoreClientBuilder
+                {
+                    Credential = googleCredential
+                }.Build();
+                var firestoreDb = FirestoreDb.Create(projectId, client: firestoreClient);
+                builder.Services.AddSingleton(firestoreDb);
+                Console.WriteLine("Connected to Firestore successfully.");
+            }
+            catch
+            {
+                // Stop the program if the connection to Firestore fails
+                Console.WriteLine("Failed to connect to Firestore.");
+                throw;
+            }
 
             // Add services to the container.
 
